88da2542f6252757ae153239a2ec1709dd5055b6
Swarup Sutradhar <sutradharswarup2007@gmail.com>
2025-11-10 19:34:52 +0530

    Add files via upload

---
+++ decimal to binary converstion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int num;
+	int arr[10];
+	int i,j=0;
+	printf("Enter the number : ");
+	scanf("%d",&num);
+	while(num>0)
+	{
+	arr[i]= num % 2;
+	num = num /2;
+	i++;
+    }
+    for(j=i-1;j>=0;j--)
+    {
+      printf("%d",arr[j]);
+    }
+	return 0;
+}
