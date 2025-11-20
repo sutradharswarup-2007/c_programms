499faaa1981d932c25ae402213846d5e10075b50
Swarup Sutradhar <sutradharswarup2007@gmail.com>
2025-11-20 20:00:31 +0530

    Add files via upload

---
+++ file handelling(using word extaction and use read print vowel word).c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+void cxtraction(char sentence[])
+{
+	char words[20][50];
+	char vowel[11]="AEIOUaeiou";
+	int i,j,len=0,w=0,c=0;
+	while (sentence[i] != '\0') 
+	{
+        if (sentence[i] == ' ' || sentence[i] == '\n') 
+		{
+            if (c > 0) 
+			{   
+			    words[w][c] = '\0';
+                w++;
+                c = 0;
+            }
+        } 
+		else
+		{
+			words[w][c++]=sentence[i];
+		}
+		i++;
+	}
+    for (i = 0; i < w; i++)
+    {
+		for(j=0;j<strlen(vowel);j++)
+		{
+		   if(words[i][0]==vowel[j])
+		   {
+			  printf("%s\n", words[i]);
+		   }		
+	 	}
+	}
+}
+int main()
+{
+	FILE *fptr;
+	char line;
+	char sentence[200];
+	
+	fptr = fopen("C:\\file\\program.txt", "r");
+	if (fptr == NULL)
+	{
+		printf("File not found :");
+		return 1;
+	}
+	printf("Words:\n");
+	while (fgets(sentence, sizeof(sentence), fptr) != '\0')
+	{
+	    cxtraction(sentence);
+	}
+	
+	fclose(fptr);
+	return 0;
+}
